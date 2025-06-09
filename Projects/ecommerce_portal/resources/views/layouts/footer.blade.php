ef4fc8fd5322feab3c212172fad4aa1d70f5f0d1
Rashed Miya <132655219+rashedsmiya@users.noreply.github.com>
2025-06-09 16:07:28 +0600

    Add All File

--- Projects/ecommerce_portal/resources/views/layouts/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="text-center p-3 theme-footer text-light">
-    © 2024 E Commerce. All Right Reserved. Designed and Developed by Code Artisan Lab.
-</footer>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
-
-</body>
-</html>
